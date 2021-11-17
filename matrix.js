ae943d42ce47767ded4eaca608e375b8c190e773
nikhil-katte <nikhilkatte7039@gmail.com>
2021-11-17 23:35:29 +0530

    Improved matrix library

--- matrix.js
+++ matrix.js
@@ -15,7 +15,7 @@ class Matrix {
     let result = new Matrix(this.cols, this.rows);
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        result.matrix[j][i] = this.data[i][j];
+        result.data[j][i] = this.data[i][j];
       }
     }
     return result;
@@ -33,7 +33,7 @@ class Matrix {
     if (n instanceof Matrix) {
       for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols; j++) {
-          this.data[i][j] += n.matrix[i][j];
+          this.data[i][j] += n.data[i][j];
         }
       }
     } else {
@@ -45,38 +45,49 @@ class Matrix {
     }
   }
 
+  static multiply(a, b) {
+    if (a.cols !== b.rows) {
+      console.log(
+        "Number of Columns and Rows of A must match Columns and Rows of B."
+      );
+      return;
+    }
+    let result = new Matrix(a.rows, b.cols);
+
+    for (let i = 0; i < result.rows; i++) {
+      for (let j = 0; j < result.cols; j++) {
+        let sum = 0;
+        for (let k = 0; k < a.cols; k++) {
+          sum += a.data[i][k] * b.data[k][j];
+        }
+        result.data[i][j] = sum;
+      }
+    }
+
+    return result;
+  }
+
   multiply(n) {
     if (n instanceof Matrix) {
-      if (this.rows !== n.rows || this.cols !== n.cols) {
-        console.log(
-          "Number of Columns and Rows of A must match Columns and Rows of B."
-        );
-        return;
-      }
-      let result = new Matrix(this, rows, n.cols);
-      let a = this;
-      let b = n;
-      for (let i = 0; i < result.rows; i++) {
-        for (let j = 0; j < result.cols; j++) {
-          let sum = 0;
-          for (let k = 0; k < a.cols; k++) {
-            sum += a.matrix[i][k] * b.matrix[k][j];
-          }
-          result.matrix[i][j] = sum;
-        }
-      }
-
-      // for (let i = 0; i < this.rows; i++) {
-      //   for (let j = 0; j < this.cols; j++) {
-      //     this.data[i][j] *= n.matrix[i][j];
-      //   }
-      // }
-      return result;
     } else {
       for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.cols; j++) {
           this.data[i][j] *= n;
         }
+      }
+    }
+  }
+
+  print() {
+    console.table(this.data);
+  }
+  map(func) {
+    //apply a funtion to every element of matrix
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let val = this.data[i][j];
+        this.data[i][j] = func(val);
       }
     }
   }
--- sketch.js
+++ sketch.js
@@ -4,6 +4,15 @@ function setup() {
   createCanvas(400, 600);
   bird = new Bird();
   pipes.push(new Pipe());
+
+  let a = new Matrix(2, 2);
+  a.randomize();
+  a.print();
+  function double(x) {
+    return x * 2;
+  }
+  a.map(double);
+  a.print();
 }
 
 function draw() {
